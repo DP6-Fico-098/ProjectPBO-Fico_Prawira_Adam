17ee0976d12d44dc8fbb80ab5422a3a2a4b4598a
dp6adamali082 <adamnasution2018@gmail.com>
2024-03-23 15:48:01 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,17 @@
-public class Main {
+    public class Main {
     public static void main(String[] args) {
-        Mahasiswa mahasiswa1 = new Mahasiswa("Fico", 19, "221401098", "Ilmu Komputer", 3.5,3.7, 3.9);
-        Mahasiswa mahasiswa2 = new Mahasiswa("Prawira", 19, "22140113", "Ilmu Komputer", 3.6, 3.8, 3.65);
+        Mahasiswa mahasiswa1 = new Mahasiswa("Fico", 19, "221401098", "Ilmu Komputer", 3.0,3.7, 3.9);
+        Mahasiswa mahasiswa2 = new Mahasiswa("Prawira", 19, "22140113", "Ilmu Komputer", 3.5, 3.8, 3.65);
         Mahasiswa mahasiswa3 = new Mahasiswa("Adam", 19, "221401082", "Ilmu Komputer", 4.0, 3.78, 3.5);
 
         mahasiswa1.info();
         mahasiswa1.jumlahIPK();
         System.out.println();
         mahasiswa2.info();
-        mahasiswa2.jumlahIPK();
+        mahasiswa2.jumlahIPK(3.7);
         System.out.println();
         mahasiswa3.info();
-        mahasiswa3.jumlahIPK();
+        mahasiswa3.jumlahIPK(3.8, 3.75);
         System.out.println();
 
     }
