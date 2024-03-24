55eeca6b3ec4e4693f5eae48ea95472714140572
Notuwapu <prawirasaragih1@gmail.com>
2024-03-24 08:23:00 +0700

    Bagian Mahasiswa

---
+++ Mahasiswa.java
@@ -0,0 +1,20 @@
+public class Mahasiswa {
+    String nama;
+    int umur;
+    String NIM;
+    String jurusan;
+    double IP1;
+    double IP2;
+    double IP3;
+
+    Mahasiswa(String nama, int umur, String NIM, String jurusan, double IP1, double IP2, double IP3) {
+        this.nama = nama;
+        this.umur = umur;
+        this.NIM = NIM;
+        this.jurusan = jurusan;
+        this.IP1 = IP1;
+        this.IP2 = IP2;
+        this.IP3 = IP3;
+
+    }
+}
